ba3df42e40193024f7a1cc394a1f05a0acfe2415
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-07 17:43:33 -0500

    [FIX] format

--- src/models/repository.ts
+++ src/models/repository.ts
@@ -1,34 +1,27 @@
-import { Sequelize, DataTypes } from 'sequelize-cockroachdb';
+import { DataTypes } from "sequelize-cockroachdb";
+import { client } from "../config/database";
 
-export class RepositoryModel{
-    constructor(private _sequelize:Sequelize){}
-    
-    buildModel(){
-        const repositoryModel = this._sequelize.define('repository',{
-            id_repository:{
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            name:{
-                type:DataTypes.STRING(50),
-                allowNull: false,
-            },
-            state:{
-                type: DataTypes.ENUM('E','D','A'),
-                allowNull: false,
-            },
-            create_time:{
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
-            status:{
-                type: DataTypes.ENUM('A','I'),
-                allowNull: false,
-            }
-        });
-        return repositoryModel;
-    }
-};
-
+export const repositoryModel = client.define("repository", {
+	id_repository: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull: false,
+	},
+	name: {
+		type: DataTypes.STRING(50),
+		allowNull: false,
+	},
+	state: {
+		type: DataTypes.ENUM("E", "D", "A"),
+		allowNull: false,
+	},
+	create_time: {
+		type: DataTypes.DATE,
+		allowNull: false,
+	},
+	status: {
+		type: DataTypes.ENUM("A", "I"),
+		allowNull: false,
+	},
+});
--- src/models/tribe.ts
+++ src/models/tribe.ts
@@ -1,27 +1,25 @@
-import { Sequelize, DataTypes } from 'sequelize-cockroachdb';
+import { Sequelize, DataTypes } from "sequelize-cockroachdb";
 
-export class TribeModel{
+export class TribeModel {
+	constructor(private _sequelize: Sequelize) {}
 
-    constructor(private _sequelize:Sequelize){}
-    
-    buildModel(){
-        const tribeModel = this._sequelize.define('tribe',{
-            id_tribe:{
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            name:{
-                type:DataTypes.STRING(50),
-                allowNull: false,
-            },
-            status:{
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            }
-        });
-        return tribeModel;
-    }
-};
-
+	buildModel() {
+		const tribeModel = this._sequelize.define("tribe", {
+			id_tribe: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			status: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		});
+		return tribeModel;
+	}
+}
