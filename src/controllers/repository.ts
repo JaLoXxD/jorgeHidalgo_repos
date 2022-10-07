866b9aa35fe2e5aed15fd1a6a99a23e38d8e049b
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-07 17:42:31 -0500

    [ADD] mock function and create repository

--- src/controllers/repository.ts
+++ src/controllers/repository.ts
@@ -1,14 +1,59 @@
-import { Model, ModelStatic } from 'sequelize-cockroachdb';
+import { Request, Response } from "express";
+import { repositoryModel } from "../models/repository";
 
-export class RepositoryController{
-    constructor(private _repositoryModel: ModelStatic<Model>){}
+//MOCK DATA
+const repositories = [
+	{
+		id: 1,
+		state: 604,
+	},
+	{
+		id: 2,
+		state: 605,
+	},
+	{
+		id: 3,
+		state: 606,
+	},
+];
 
-    createRepositories(){
-        this._repositoryModel.create({
-            name:'test',
-            state:'D',
-            create_time:new Date(),
-            status:'A'
-        })
-    }
-}
+const createRepositories = async (req: Request, res: Response) => {
+	try {
+		await repositoryModel.create({
+			name: "test",
+			state: "D",
+			create_time: new Date(),
+			status: "A",
+		});
+		return res.status(200).json({
+			success: true,
+			message: "Repository created successfully.",
+		});
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({
+			success: false,
+			message: "An error was ocurred in the repository creation.",
+			err,
+		});
+	}
+};
+
+const mockRepositories = async (req: Request, res: Response) => {
+	try {
+		return res.status(200).json({
+			success: true,
+			message: "Mock obtained successfully.",
+			repositories,
+		});
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({
+			success: false,
+			message: "An error was ocurred getting the repositories mock.",
+			err,
+		});
+	}
+};
+
+export { createRepositories, mockRepositories };
