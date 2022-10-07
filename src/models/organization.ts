f37579a58cfbd3f1bbdc0d6c6b9455ea00d0c7ff
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-07 17:43:19 -0500

    [FIX] format

--- src/models/organization.ts
+++ src/models/organization.ts
@@ -1,26 +1,25 @@
-import { Sequelize, DataTypes } from 'sequelize-cockroachdb';
+import { Sequelize, DataTypes } from "sequelize-cockroachdb";
 
-export class OrganizationModel{
-    constructor(private _sequelize:Sequelize){}
-    
-    buildModel(){
-        const organizationModel = this._sequelize.define('organization',{
-            id_organization:{
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            name:{
-                type:DataTypes.STRING(50),
-                allowNull: false,
-            },
-            status:{
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            }
-        });
-        return organizationModel;
-    }
-};
+export class OrganizationModel {
+	constructor(private _sequelize: Sequelize) {}
 
+	buildModel() {
+		const organizationModel = this._sequelize.define("organization", {
+			id_organization: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			status: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		});
+		return organizationModel;
+	}
+}
