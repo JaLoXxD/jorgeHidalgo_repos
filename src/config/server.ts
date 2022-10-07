4d011cd7fa93fd971306bc79a7fce4ee8654a882
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-07 17:42:12 -0500

    [UPD] routes and models

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,21 +2,13 @@ import * as express from "express";
 import { Express /*,  Request, Response  */ } from "express";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
-import { Database } from "./database";
-import { OrganizationModel } from "../models/organization";
-import { Sequelize } from "sequelize-cockroachdb";
-import { TribeModel } from "../models/tribe";
-import { RepositoryModel } from '../models/repository';
-import { MetricsModel } from '../models/metrics';
+import { client, Database } from "./database";
+import { router } from "../routes/index";
 
 export class Server {
 	private _port: string | number;
 	private _app: Express = express();
 	private _database: Database = new Database();
-	private _organizationModel: OrganizationModel;
-	private _tribeModel: TribeModel;
-    private _repositoryModel: RepositoryModel;
-    private _metricsModel: MetricsModel;
 
 	constructor() {}
 
@@ -31,30 +23,35 @@ export class Server {
 		this._app.set("port", this._port);
 	}
 
-	private _buildModels(sequelize: Sequelize) {
-		this._organizationModel = new OrganizationModel(sequelize);
-		const organization = this._organizationModel.buildModel();
-		this._tribeModel = new TribeModel(sequelize);
-		const tribe = this._tribeModel.buildModel();
-        this._repositoryModel = new RepositoryModel(sequelize);
-        const repository = this._repositoryModel.buildModel();
-        this._metricsModel = new MetricsModel(sequelize);
-        const metrics = this._metricsModel.buildModel();
-        /* RELATIONSHIP */
-		organization.hasMany(tribe);
-        tribe.hasMany(repository);
-        repository.hasMany(metrics);
-        console.log(typeof organization)
-	}
+	// private _buildModels(sequelize: Sequelize) {
+	//     return new Promise((resolve, reject)=>{
+	//         this._organizationModel = new OrganizationModel(sequelize);
+	//         const organization = this._organizationModel.buildModel();
+	//         this._tribeModel = new TribeModel(sequelize);
+	//         const tribe = this._tribeModel.buildModel();
+	//         this._repositoryModel = new RepositoryModel(sequelize);
+	//         this._repository = this._repositoryModel.buildModel();
+	//         this._metricsModel = new MetricsModel(sequelize);
+	//         const metrics = this._metricsModel.buildModel();
+	//         /* RELATIONSHIP */
+	//         organization.hasMany(tribe);
+	//         tribe.hasMany(this._repository);
+	//         this._repository.hasMany(metrics);
+	//         console.log('finish')
+	//         resolve(true);
+	//     })
+	// }
 
 	startServer() {
 		this._setMiddlewares();
-		this._setPort();
 		this._database.startConnection();
-		this._buildModels(this._database.client);
+		this._setPort();
+		/* this._buildModels(this._database.client).then(()=>{
+        }); */ this._app.use(router);
 		this._app.listen(this._app.get("port"), () => {
 			console.log(`Server listening on port ${this._app.get("port")}`);
 		});
-		this._database.client.sync({ force: true });
+		// this._database.client.sync({ force: true });
+		client.sync({ force: true });
 	}
 }
