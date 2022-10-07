d7820b82e002d20d143691f9a53a19958ff8e13f
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-07 17:43:08 -0500

    [FIX] format

--- src/models/metrics.ts
+++ src/models/metrics.ts
@@ -1,39 +1,37 @@
-import { Sequelize, DataTypes } from 'sequelize-cockroachdb';
+import { Sequelize, DataTypes } from "sequelize-cockroachdb";
 
-export class MetricsModel{
+export class MetricsModel {
+	constructor(private _sequelize: Sequelize) {}
 
-    constructor(private _sequelize:Sequelize){}
-    
-    buildModel(){
-        const metricsModel = this._sequelize.define('metrics',{
-            id_repository:{
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            coverage:{
-                type:DataTypes.DOUBLE,
-                allowNull: false,
-            },
-            bugs:{
-                type:DataTypes.INTEGER,
-                allowNull: false,
-            },
-            vulnerabilities:{
-                type:DataTypes.INTEGER,
-                allowNull: false,
-            },
-            hotspot:{
-                type:DataTypes.INTEGER,
-                allowNull: false,
-            },
-            code_smells:{
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            }
-        });
-        return metricsModel;
-    }
-};
-
+	buildModel() {
+		const metricsModel = this._sequelize.define("metrics", {
+			id_repository: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			coverage: {
+				type: DataTypes.DOUBLE,
+				allowNull: false,
+			},
+			bugs: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			vulnerabilities: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			hotspot: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			code_smells: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+		});
+		return metricsModel;
+	}
+}
