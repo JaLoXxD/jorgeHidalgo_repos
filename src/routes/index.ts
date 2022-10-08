abab11f0e81bcd5599425b087eb8f82653d4bdea
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-08 18:37:47 -0500

    [ADD] routes and middlewares

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,20 @@
 import Router from "express-promise-router";
-import { createRepositories, mockRepositories } from "../controllers/repository";
+import { checkIfTribeExists, checkIfOrganizationExists } from "../middlewares";
+import { findMetricsByTribe, generateCsv } from "../controllers/metrics";
+import { createOrganization, deleteOrganization, getOrganizations, updateOrganization } from "../controllers/organization";
+import { createMockRepositories, mockRepositories } from "../controllers/repository";
 
 export const router = Router();
 
-router.post("/api/v1/repository", createRepositories);
+/* TASK 1 MOCK */
+router.post("/api/v1/repository-mock", createMockRepositories);
 router.get("/api/v1/repository-mock", mockRepositories);
+/* TASK 2 CRUD */
+router.get("/api/v1/organization", getOrganizations);
+router.post("/api/v1/organization", createOrganization);
+router.put("/api/v1/organization/:id", checkIfOrganizationExists, updateOrganization);
+router.delete("/api/v1/organization/:id", checkIfOrganizationExists, deleteOrganization);
+/* TASK 3 METRICS */
+router.get("/api/v1/metrics/:id", checkIfTribeExists, findMetricsByTribe);
+/* TASK 4 CSV */
+router.get("/api/v1/metrics-csv/:id", generateCsv);
