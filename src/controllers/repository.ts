48edf730ffbdfe58da9c63a5ad7e8b438d04e68a
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-08 18:35:37 -0500

    [ADD] repository functions and models
    models and functions to create repositories and mock

--- src/controllers/repository.ts
+++ src/controllers/repository.ts
@@ -1,39 +1,62 @@
 import { Request, Response } from "express";
 import { repositoryModel } from "../models/repository";
+import { createMockTribe } from "./tribe";
+import { createMetricsBody } from "./metrics";
 
 //MOCK DATA
-const repositories = [
+const repositories: Array<createRepositoryBody> = [
 	{
-		id: 1,
-		state: 604,
+		id_repository: 1,
+		name: "cd-common-utils",
+		state: "E",
+		status: "A",
 	},
 	{
-		id: 2,
-		state: 605,
+		id_repository: 2,
+		name: "cd-common-text",
+		state: "D",
+		status: "I",
 	},
 	{
-		id: 3,
-		state: 606,
+		id_repository: 3,
+		name: "cd-common-test",
+		state: "A",
+		status: "A",
 	},
 ];
 
-const createRepositories = async (req: Request, res: Response) => {
+export interface createRepositoryBody {
+	id_repository?: number;
+	name: string;
+	state: "E" | "D" | "A";
+	create_time?: Date;
+	status: "A" | "I";
+	metrics?: createMetricsBody;
+}
+
+const stateEnum = {
+	E: 604,
+	D: 605,
+	A: 606,
+};
+
+const createRepository = async (tribeId: number, body: createRepositoryBody, res: Response) => {
 	try {
-		await repositoryModel.create({
-			name: "test",
-			state: "D",
-			create_time: new Date(),
-			status: "A",
+		const newRepository = await repositoryModel.create({
+			name: body.name,
+			state: body.state,
+			create_time: body.create_time,
+			status: body.status,
+			id_tribe: tribeId,
 		});
-		return res.status(200).json({
-			success: true,
-			message: "Repository created successfully.",
-		});
+		await newRepository.save();
+		const savedRepository = await newRepository.get();
+		return savedRepository;
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json({
 			success: false,
-			message: "An error was ocurred in the repository creation.",
+			message: "An error ocurred in the repository creation.",
 			err,
 		});
 	}
@@ -41,19 +64,55 @@ const createRepositories = async (req: Request, res: Response) => {
 
 const mockRepositories = async (req: Request, res: Response) => {
 	try {
+		const reposData = await repositoryModel.findAll({
+			attributes: [["id_repository", "id"], "state"],
+			replacements: ["D"],
+		});
+
+		const reposMock = reposData.map((repo) => {
+			const repoData = repo.get();
+			return { ...repoData, state: stateEnum[repoData.state] };
+		});
+
+		console.log(reposMock);
+
 		return res.status(200).json({
 			success: true,
 			message: "Mock obtained successfully.",
-			repositories,
+			repositories: reposMock,
 		});
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json({
 			success: false,
-			message: "An error was ocurred getting the repositories mock.",
+			message: "An error ocurred getting the repositories mock.",
 			err,
 		});
 	}
 };
 
-export { createRepositories, mockRepositories };
+const createMockRepositories = async (req: Request, res: Response) => {
+	try {
+		const mockTribe = await createMockTribe(res);
+		for (const repository of repositories) {
+			repositoryModel.create({
+				...repository,
+				create_time: new Date(),
+				id_tribe: mockTribe.id_tribe,
+			});
+		}
+		res.status(200).json({
+			success: true,
+			message: "Mock data created successfully",
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			success: false,
+			message: "An error ocurred creating the mock data.",
+			err,
+		});
+	}
+};
+
+export { createRepository, mockRepositories, createMockRepositories };
