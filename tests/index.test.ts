4a884d8c94d746dd025ed55e50897e5a82927d5a
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-08 18:38:02 -0500

    [ADD] testing for task 3

---
+++ tests/index.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import { Server } from "../src/config/server";
+import { client } from "../src/config/database";
+
+describe("task 3 tests", () => {
+	const app = new Server();
+	beforeAll(async () => {
+		await client.sync({ force: false });
+		await client.authenticate();
+        jest.setTimeout(30000);
+	});
+
+	it("should return an error if the tribe doesn't have repositories that  ", async () => {
+		const req = await request(app.startTestingServer()).get("/api/v1/metrics/803459643706867713");
+		expect(req.statusCode).toBe(500);
+		expect(req.body.success).toEqual(false);
+		expect(req.body.message).toEqual("The tribe doesn't have repositories that meet the necessary coverage");
+	});
+
+	it("should return an array of repositories", async () => {
+		const req = await request(app.startTestingServer()).get("/api/v1/metrics/803457101829603329");
+		expect(req.statusCode).toBe(200);
+		expect(req.body.success).toEqual(true);
+		expect(req.body.message).toEqual("Metrics obtained successfully.");
+		expect(req.body).toHaveProperty("repositories");
+	});
+
+	it("should return an erroy if the tribe is not registered", async () => {
+		const req = await request(app.startTestingServer()).get("/api/v1/metrics/512");
+		console.log(req.body);
+		expect(req.statusCode).toBe(400);
+		expect(req.body.success).toEqual(false);
+		expect(req.body.message).toEqual("The tribe is not registered");
+	});
+});
