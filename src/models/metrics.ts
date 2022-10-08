fed16e0fe4c78f96c38d14964f803376468a2a71
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-08 18:37:20 -0500

    [UPD] use arrow function instead class

--- src/models/metrics.ts
+++ src/models/metrics.ts
@@ -1,37 +1,35 @@
-import { Sequelize, DataTypes } from "sequelize-cockroachdb";
+import { DataTypes, ModelStatic, Model } from "sequelize-cockroachdb";
+import { client } from "../config/database";
 
-export class MetricsModel {
-	constructor(private _sequelize: Sequelize) {}
-
-	buildModel() {
-		const metricsModel = this._sequelize.define("metrics", {
-			id_repository: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-				autoIncrement: true,
-				allowNull: false,
-			},
-			coverage: {
-				type: DataTypes.DOUBLE,
-				allowNull: false,
-			},
-			bugs: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-			vulnerabilities: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-			hotspot: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-			code_smells: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-		});
-		return metricsModel;
-	}
-}
+export const metricsModel: ModelStatic<Model> = client.define(
+	"metrics",
+	{
+		id_repository: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false,
+		},
+		coverage: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		bugs: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		vulnerabilities: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		hotspot: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		code_smells: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},
+	{ createdAt: false, updatedAt: false }
+);
