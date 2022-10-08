7564db421271df396f9725939e279f83721eb37c
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-10-08 18:36:11 -0500

    [ADD] enums
    enums used to map result

---
+++ src/data/constants.ts
@@ -0,0 +1,12 @@
+const verificationStateEnum = {
+	E: "Verificado",
+	D: "En espera",
+	A: "Aprobado",
+};
+
+const stateEnum = {
+	A: "Habilitado",
+	I: "Archivado",
+};
+
+export { verificationStateEnum, stateEnum };
